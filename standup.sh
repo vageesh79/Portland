81ecf1dcfe41ebe4df0ddd8e07b524da4a2097d7
Wolfereign <nvonwolff@gmail.com>
2017-11-21 17:18:34 -0700

    Generilize user name

--- standup.sh
+++ standup.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+
 # System configuration for my home server (run as root)(Ubuntu Server)
 # Zero to Operational
 
 # Install options/roles should be UEFI, Standard System Utilities, OpenSSH Server
 
-# get SSH credentials
-read -p 'Username for SSH: ' sshUser
-read -p 'Public Key for SSH: ' sshPubKey
+# get Username for primary User
+read -pr 'Primary Username: ' user
+
+# get SSH public key for primary user
+read -prs 'Public Key for SSH: ' sshPubKey
+
+
+# get SNMTP credentials
+#read -pr 'SMTP User: ' smtpUser
+#read -prs 'SMTP Password: ' smtpPass
 
 # Run updates
 apt update -y
@@ -14,6 +23,11 @@ apt upgrade -y
 # Install ZFS and Impot zpool(vpool)
 apt install -y zfsutils-linux
 zpool import vpool
+
+# Setup ZFS Notifications
+#mail relay
+#user input for smtp password
+#zfs notification service
 
 # Install Docker
 apt install -y apt-transport-https ca-certificates curl software-properties-common
@@ -30,52 +44,41 @@ zfs create vpool/docker
 systemctl stop docker
 mkdir /etc/systemd/system/docker.service.d
 touch /etc/systemd/system/docker.service.d/docker.conf
-echo "[Service]" >> /etc/systemd/system/docker.service.d/docker.conf
-echo "ExecStart=" >> /etc/systemd/system/docker.service.d/docker.conf
-echo "ExecStart=/usr/bin/dockerd --graph="/vpool/docker" --storage-driver=zfs" >> /etc/systemd/system/docker.service.d/docker.conf
+cat << 'EOF' >> /etc/systemd/system/docker.service.d/docker.conf
+    "[Service]"
+    "ExecStart="
+    "ExecStart=/usr/bin/dockerd --graph="/vpool/docker" --storage-driver=zfs"
+EOF
 systemctl daemon-reload
 systemctl start docker
 
 # Setup Docker MacVLan (add ipv6 later)
 docker network create -d macvlan  \
-    --subnet=$(ip route get 8.8.8.8 | cut -d ' ' -f 3 | cut -d '.' -f 1-3).0/24  \
-    --gateway=$(ip route get 8.8.8.8 | cut -d ' ' -f 3)  \
-    -o parent=$(ip route get 8.8.8.8 | cut -d ' ' -f 5 | head -1) macvlan0
+    --subnet="$(ip route get 8.8.8.8 | cut -d ' ' -f 3 | cut -d '.' -f 1-3).0/24"  \
+    --gateway="$(ip route get 8.8.8.8 | cut -d ' ' -f 3)"  \
+    -o parent="$(ip route get 8.8.8.8 | cut -d ' ' -f 5 | head -1)" macvlan0
 
 # Create Docker Management Group and Add User
 groupadd docker
-usermod -aG docker wolfereign
+usermod -aG docker "$user"
 
 # Create media library user (curator)
 useradd -u 6846 curator
-echo curator:$(openssl rand -base64 32) | chpasswd
-usermod -aG curator wolfereign
+echo curator:"$(openssl rand -base64 32)" | chpasswd
+usermod -aG curator "$user"
 
 # Fix permissions on Library SubVolume
 chown -R curator:curator /vpool/library
 chmod -R 770 /vpool/library
 chmod -R g+s /vpool/library
 
-# Setup/Run Plex Docker Container
-
-# Setup/Run Deluge Docker Container
-
-# Setup/Run Samba Docker Container
-
-# Setup/Run SMTP Mail Server Docker Container
-
-# Setup ZFS Notifications
-#mail relay
-#user input for smtp password
-#zfs notification service
-
 # Setup SSH for main user
-mkdir /home/$sshUser/.ssh
-touch /home/$sshUser/.ssh/authorized_keys
-chown -R $sshUser:$sshUser /home/$sshUser/.ssh
-chmod 700 /home/$sshUser/.ssh
-chmod 600 /home/$sshUser/.ssh/authorized_keys
-cat $sshPubKey >> /home/$sshUser/.ssh/authorized_keys
+mkdir "/home/$user/.ssh"
+touch "/home/$user/.ssh/authorized_keys"
+chown -R "$user:$user /home/$user/.ssh"
+chmod 700 "/home/$user/.ssh"
+chmod 600 "/home/$user/.ssh/authorized_keys"
+cat "$sshPubKey" >> "/home/$user/.ssh/authorized_keys"
 
 # Change SSH Settings
 sed -i  'Port 22/ c\Port 8668' /etc/ssh/sshd_config
