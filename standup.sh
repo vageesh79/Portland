31ab10399d6bf1697c31c4d636d580cbcbb84009
Wolfereign <nvonwolff@gmail.com>
2017-11-21 19:31:40 -0700

    reorganize a bit
    just some  order of operations for my sanity

--- standup.sh
+++ standup.sh
@@ -29,6 +29,22 @@ zpool import vpool
 #user input for smtp password
 #zfs notification service
 
+# Setup SSH for main user
+mkdir "/home/$user/.ssh"
+touch "/home/$user/.ssh/authorized_keys"
+chown -R "$user:$user /home/$user/.ssh"
+chmod 700 "/home/$user/.ssh"
+chmod 600 "/home/$user/.ssh/authorized_keys"
+cat "$sshPubKey" >> "/home/$user/.ssh/authorized_keys"
+
+# Change SSH Settings
+sed -i  'Port 22/ c\Port 8668' /etc/ssh/sshd_config
+sed -i  'PermitRootLogin/ c\PermitRootLogin no' /etc/ssh/sshd_config
+sed -i  'PubkeyAuthentication/ c\PubkeyAuthentication yes' /etc/ssh/sshd_config
+sed -i  'AuthorizedKeysFile/ c\AuthorizedKeysFile %h/.ssh/authorized_keys' /etc/ssh/sshd_config
+sed -i  'PasswordAuthentication/ c\PasswordAuthentication no' /etc/ssh/sshd_config
+systemctl restart sshd
+
 # Install Docker
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -44,10 +60,10 @@ zfs create vpool/docker
 systemctl stop docker
 mkdir /etc/systemd/system/docker.service.d
 touch /etc/systemd/system/docker.service.d/docker.conf
-cat << 'EOF' >> /etc/systemd/system/docker.service.d/docker.conf
-    "[Service]"
-    "ExecStart="
-    "ExecStart=/usr/bin/dockerd --graph="/vpool/docker" --storage-driver=zfs"
+cat << EOF >> /etc/systemd/system/docker.service.d/docker.conf
+    [Service]
+    ExecStart=
+    ExecStart=/usr/bin/dockerd --graph=/vpool/docker --storage-driver=zfs
 EOF
 systemctl daemon-reload
 systemctl start docker
@@ -71,22 +87,6 @@ usermod -aG curator "$user"
 chown -R curator:curator /vpool/library
 chmod -R 770 /vpool/library
 chmod -R g+s /vpool/library
-
-# Setup SSH for main user
-mkdir "/home/$user/.ssh"
-touch "/home/$user/.ssh/authorized_keys"
-chown -R "$user:$user /home/$user/.ssh"
-chmod 700 "/home/$user/.ssh"
-chmod 600 "/home/$user/.ssh/authorized_keys"
-cat "$sshPubKey" >> "/home/$user/.ssh/authorized_keys"
-
-# Change SSH Settings
-sed -i  'Port 22/ c\Port 8668' /etc/ssh/sshd_config
-sed -i  'PermitRootLogin/ c\PermitRootLogin no' /etc/ssh/sshd_config
-sed -i  'PubkeyAuthentication/ c\PubkeyAuthentication yes' /etc/ssh/sshd_config
-sed -i  'AuthorizedKeysFile/ c\AuthorizedKeysFile %h/.ssh/authorized_keys' /etc/ssh/sshd_config
-sed -i  'PasswordAuthentication/ c\PasswordAuthentication no' /etc/ssh/sshd_config
-systemctl restart sshd
 
 # Install Cockpit Web Gui
 apt install -y cockpit cockpit-networkmanager cockpit-storaged cockpit-system cockpit-packagekit cockpit-docker
