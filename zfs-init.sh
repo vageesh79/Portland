f3e9a35d6bd295849cdd2439e22de2e57a314f9f
Wolfereign <nvonwolff@gmail.com>
2017-11-21 19:35:12 -0700

    fixing possible errors

--- zfs-init.sh
+++ zfs-init.sh
@@ -13,23 +13,23 @@ apt install -y zfsutils-linux
 lblsk
 
 # Get ZFS Options
-read -p 'ZFS - Specify desired pool name: ' RName
-read -p 'ZFS - Specify raid type (raidz, raidz2, etc..): ' RType
-read -p 'ZFS - Specify drives to add to raid (EX: /dev/sdb /dev/sdc /dev/sda): ' RDevices
+read -pr 'ZFS - Specify desired pool name: ' RName
+read -pr 'ZFS - Specify raid type (raidz, raidz2, etc..): ' RType
+read -pr 'ZFS - Specify drives to add to raid (EX: /dev/sdb /dev/sdc /dev/sda): ' RDevices
 
 # Create ZFS Raid
 zpool
 
 # Create ZFS Subvolumes and Additional Directories
-zfs create $RName/docker
-zfs create $RName/kvm
-zfs create $RName/library
-zfs create $RName/backups
-mkdir /$RName/library/videos
-mkdir /$RName/library/videos/tv_series
-mkdir /$RName/library/videos/movies
-mkdir /$RName/library/music
-mkdir /$RName/library/books
-mkdir /$RName/library/comics
-mkdir /$RName/library/temp
-mkdir /$RName/library/temp/.incomplete_torrents
+zfs create "$RName/docker"
+zfs create "$RName/kvm"
+zfs create "$RName/library"
+zfs create "$RName/backups"
+mkdir "/$RName/library/videos"
+mkdir "/$RName/library/videos/tv_series"
+mkdir "/$RName/library/videos/movies"
+mkdir "/$RName/library/music"
+mkdir "/$RName/library/books"
+mkdir "/$RName/library/comics"
+mkdir "/$RName/library/temp"
+mkdir "/$RName/library/temp/.incomplete_torrents"
