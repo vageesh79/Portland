9a8216b42e842f3dd84f2cb3fffc72ee507c99af
Wolfereign <nvonwolff@gmail.com>
2017-11-22 10:38:48 -0700

    add restart policy

--- run_container/watchtower.sh
+++ run_container/watchtower.sh
@@ -5,5 +5,6 @@
 # Stand Up New container
 docker run -d  \
   --name watchtower \
+  --restart="unless-stopped" \
   --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
   v2tec/watchtower:latest --schedule="0 2 * * *" --cleanup
