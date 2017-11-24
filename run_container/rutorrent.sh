9d98dc777e50c3c554993f4a57713136ae9f6b43
Wolfereign <nvonwolff@gmail.com>
2017-11-24 14:22:18 -0700

    fixes

--- run_container/rutorrent.sh
+++ run_container/rutorrent.sh
@@ -6,8 +6,8 @@
 #read -prs 'Password for PIA: ' PIAPass
 
 # Get WebUI Password
-read -rp 'User for WebUI' WebUser
-read -rsp 'Password for WebUI' WebPass
+read -rp 'User for WebUI: ' WebUser
+read -rsp 'Password for WebUI: ' WebPass
 
 # Stand Up new rutorrent container
 docker run -d \
@@ -18,7 +18,6 @@ docker run -d \
     --ip="$(ip route get 8.8.8.8 | cut -d ' ' -f 3 | cut -d '.' -f 1-3).67" \
     --hostname=rutorrent \
     --restart="unless-stopped" \
-    --mount type=volume,source=rutorrent-volume,target=/config \
     --mount type=bind,source=/vpool/library/temp/.rutorrent/downloads,target=/downloads \
     --mount type=bind,source=/vpool/library,target=/library \
     --mount type=bind,source=/etc/localtime,target=/etc/localtime:ro \
