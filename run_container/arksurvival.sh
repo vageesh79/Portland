be7ab288717987e027a79cc5345bf2fb70380c10
Wolfereign <Wolfereign@users.noreply.github.com>
2018-02-26 18:05:55 -0700

    Update arksurvival.sh

--- run_container/arksurvival.sh
+++ run_container/arksurvival.sh
@@ -16,11 +16,11 @@ docker run -it  \
   --ip "$(ip route get 8.8.8.8 | cut -d ' ' -f 3 | cut -d '.' -f 1-3).69" \
   --hostname ark \
   --mount type=bind,source=/vpool/docker-configs/ark,target=/ark \
-  -e SESSIONNAME=tpkark \
+  -e SESSIONNAME=TPK \
   -e SERVERMAP=TheIsland \
   -e SERVERPASSWORD=$sPass \
   -e ADMINPASSWORD=$aPass \
-  -e MAX_PLAYERS=20 \
+  -e MAX_PLAYERS=25 \
   -e TZ="America/Denver" \
   -e ARK_UID=4346 \
   -e ARK_GID=4346 \
